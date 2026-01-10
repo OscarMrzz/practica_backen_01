5374de8fbacc120eb04cd4a62c69bee1333606d4
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2026-01-09 19:20:42 -0600

    refactor(main): improve database connection check and response format
    - Properly import and configure dotenv
    - Add database connection check in root endpoint
    - Standardize string quotes and formatting
    - Improve error handling for database operations

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,39 @@
-import Fastify from 'fastify'
-import fastifyPostgres from '@fastify/postgres'
-import { productosRouter } from './app/productos/productos.router.js'
-import  'dotenv'
+import Fastify from "fastify";
+import fastifyPostgres from "@fastify/postgres";
+import { productosRouter } from "./app/productos/productos.router.js";
+import dotenv from "dotenv";
+dotenv.config();
 
+const fastify = Fastify({ logger: true });
 
-
-
-
-const fastify = Fastify({ logger: true})
-
-const dbConnectionString = process.env.db_connection_string || ''
-if (!dbConnectionString){
-    fastify.log.error('db_connection_string is not defined')
-    
+const dbConnectionString = process.env.db_connection_string || "";
+if (!dbConnectionString) {
+  fastify.log.error("db_connection_string is not defined");
 }
 fastify.register(fastifyPostgres, {
-    connectionString: dbConnectionString
-})
+  connectionString: dbConnectionString,
+});
 
-fastify.get("/",async  (req,replay)=>{
-    replay.send(
-        {
-            message:"Conexion exitosa"
-        }
-    )
+fastify.get("/", async (req, replay) => {
+  try {
+    const query = "SELECT current_timestamp;";
+    const { rows } = await fastify.pg.query(query);
+    if (rows.length === 0) {
+      replay.send({conexionBackend:"OK",conexionDB:"error"});
+    }
+      replay.send({conexionBackend:"OK",conexionDB:"OK"});
+  } catch (error) {
+    fastify.log.error(error);
+    replay.status(500).send({conexionBackend:"OK",conexionDB:"error"});
+  }
+});
 
-}
-)
-fastify.register(productosRouter,{prefix:"/api/productos"})
+fastify.register(productosRouter, { prefix: "/api/productos" });
 
+const start = async () => {
+  const port = process.env.port || 3000;
+  fastify.listen({ port: Number(port), host: "0.0.0.0" });
+  fastify.log.info(`Server listening on port ${port}`);
+};
 
-const start = async ()=>{
-    const  port = process.env.port || 3000
-    fastify.listen({port:Number(port), host:'0.0.0.0'})
-    fastify.log.info(`Server listening on port ${port}`)
-}
-
-start()
+start();
