df9455bed34d6baf82d36bcc27900ecac1662e9f
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2026-01-09 19:20:11 -0600

    refactor(productos): simplify route paths by removing redundant prefix
    The "/productos" prefix was removed from all route paths since the router is already mounted under "/productos". This makes the routes more concise and avoids redundancy.

--- src/app/productos/productos.router.ts
+++ src/app/productos/productos.router.ts
@@ -1,12 +1,13 @@
+
 import type { FastifyInstance } from "fastify"
 import { createProducto, deleteByIdProducto, getAllProductos, getByIdProducto, updateByIdProducto } from "./productos.controller.js"
 
 
 
 export async function productosRouter(fastify:FastifyInstance){
-    fastify.get("/productos",getAllProductos)
-    fastify.get("/productos/:idProducto",getByIdProducto)
-    fastify.post("/productos",createProducto)
-    fastify.put("/productos/:idProducto",updateByIdProducto)
-    fastify.delete("/productos/:idProducto",deleteByIdProducto)
+    fastify.get("/",getAllProductos)
+    fastify.get("/:idProducto",getByIdProducto)
+    fastify.post("/",createProducto)
+    fastify.put("/:idProducto",updateByIdProducto)
+    fastify.delete("/:idProducto",deleteByIdProducto)
 }
