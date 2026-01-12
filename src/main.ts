cabe12f561903629a32fecd9d920f950c4484f32
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2026-01-11 22:33:24 -0600

    docs: update test comment in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { MiServidor } from "./server.js";
 import type { FastifyInstance } from "fastify";
 
-/* PRUEBA 1 */
+/* PRUEBA 3 */
 const start = async (servidor: FastifyInstance) => {
   const port = process.env.port || 3000;
   servidor.listen({ port: Number(port), host: "0.0.0.0" });
