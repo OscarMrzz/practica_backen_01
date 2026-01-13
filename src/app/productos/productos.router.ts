0c932ca2a8611d8d94e28f504c214cd6cd4b163d
Oscar Jossue Hernadez Rodriguez <osscarodriguez@gmail.com>
2026-01-12 18:42:08 -0600

    Solucionando el erro 6 (son pruebas)

--- src/app/productos/productos.router.ts
+++ src/app/productos/productos.router.ts
@@ -7,7 +7,7 @@ import { createProducto, deleteByIdProducto, getAllProductos, getByIdProducto, u
 export async function productosRouter(fastify:FastifyInstance){
     fastify.get("/",getAllProductos)
     fastify.get("/:id_producto",getByIdProducto)
-    fastify.post("/m",createProducto)
+    fastify.post("/",createProducto)
     fastify.put("/:id_producto",updateByIdProducto)
     fastify.delete("/:id_producto",deleteByIdProducto)
 }
